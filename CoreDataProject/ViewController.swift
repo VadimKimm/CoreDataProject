7d089e6b82ca024614cab8dd2fa26bdf8ec85db2
Vadim <vadim_kimm@icloud.com>
2022-08-14 13:36:48 +0300

    Initial Commit

---
+++ CoreDataProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataProject
+//
+//  Created by Vadim Kim on 14.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
