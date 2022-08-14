a5326b4f000c9e8eb123ea8d623809610aa28713
Vadim <vadim_kimm@icloud.com>
2022-08-14 15:40:40 +0300

    Created class UsersView

--- CoreDataProject/Sources/App/SceneDelegate.swift
+++ CoreDataProject/Sources/App/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
 
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = UsersViewController()
+        window.rootViewController = UINavigationController(rootViewController: UsersViewController())
         window.makeKeyAndVisible()
         self.window = window
     }
--- CoreDataProject/Sources/Scenes/UsersModule/View/UsersViewController.swift
+++ CoreDataProject/Sources/Scenes/UsersModule/View/UsersViewController.swift
@@ -9,9 +9,72 @@ import UIKit
 
 class UsersViewController: UIViewController {
 
+    var models = ["test1", "test2", "test3"]
+
+    // MARK: - Properties
+
+    private var usersView: UsersView? {
+        guard isViewLoaded else { return nil }
+        return view as? UsersView
+    }
+
+    // MARK: - Lifecycle
+
+    override func loadView() {
+        view = UsersView()
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .blue
+        setupView()
+    }
+
+    // MARK: - Private functions
+
+    private func setupView() {
+        title = "Users"
+        navigationController?.navigationBar.prefersLargeTitles = true
+
+        usersView?.tableView.delegate = self
+        usersView?.tableView.dataSource = self
+        usersView?.addUserTextField.delegate = self
+        usersView?.tableView.keyboardDismissMode = .onDrag
     }
 }
 
+// MARK: - UITableViewDataSource
+
+extension UsersViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        models.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let model = models[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.accessoryType = .disclosureIndicator
+        cell.textLabel?.text = model
+        return cell
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension UsersViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let action = UIContextualAction(style: .destructive, title: "Delete") { action, view, completionHandler in
+
+        }
+
+        return UISwipeActionsConfiguration(actions: [action])
+    }
+}
+
+// MARK: - UITextFieldDelegate
+
+extension UsersViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
