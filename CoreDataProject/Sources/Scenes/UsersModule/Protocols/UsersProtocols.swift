b9dfe44b4d2decb5ebdbd29646fd2ee8875f7be0
Vadim <vadim_kimm@icloud.com>
2022-08-17 14:12:40 +0300

    Created protocols UsersViewProtocol and UsersPresenterProtocol

---
+++ CoreDataProject/Sources/Scenes/UsersModule/Protocols/UsersProtocols.swift
@@ -0,0 +1,28 @@
+//
+//  UsersProtocols.swift
+//  CoreDataProject
+//
+//  Created by Vadim Kim on 14.08.2022.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - UsersViewProtocol
+
+protocol UsersViewProtocol: AnyObject {
+    func fetchTableView()
+    func addUserButtonTapped()
+}
+
+// MARK: - UsersPresenterProtocol
+
+protocol UsersPresenterProtocol: AnyObject {
+    var users: [User]? { get set  }
+
+    init(view: UsersViewProtocol, coreDataService: CoreDataProtocol, router: UsersRouterProtocol)
+    func saveUser(_ name: String)
+    func getAllUsers()
+    func deleteUser(_ user: User)
+    func userDidSelect(user: User?)
+}
