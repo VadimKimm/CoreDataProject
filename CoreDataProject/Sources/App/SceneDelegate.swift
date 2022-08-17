ba8b2b1ab18ceed820f3d81775eb54f83c3378cf
Vadim <vadim_kimm@icloud.com>
2022-08-17 18:37:07 +0300

    Modified func scene()

--- CoreDataProject/Sources/App/SceneDelegate.swift
+++ CoreDataProject/Sources/App/SceneDelegate.swift
@@ -11,12 +11,16 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = UINavigationController(rootViewController: UsersViewController())
+
+        let navigationController = UINavigationController()
+        let assemblyModule = AssemblyModule()
+        let router = Router(navigationController: navigationController, assemblyModule: assemblyModule)
+        router.initializeViewControllers()
+
+        window.rootViewController = navigationController
         window.makeKeyAndVisible()
         self.window = window
     }
